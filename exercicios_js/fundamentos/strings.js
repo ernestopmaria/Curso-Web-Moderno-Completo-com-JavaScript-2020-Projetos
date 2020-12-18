808fdefe09915c752ea1db25f9de9de692ece7fc
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-12-18 09:00:59 +0100

    u

--- exercicios_js/fundamentos/strings.js
+++ exercicios_js/fundamentos/strings.js
@@ -2,3 +2,11 @@ const escola = "cod3r"
 
 console.log(escola.charAt(3)) // o chartAt irá mostrar a posição do da lentra na palavra
 console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(1, 3))
+console.log('Escola '.concat(escola).concat(' !'))
+console.log(escola.replace(3, 'er'))
+
+console.log('Ana , Maria , Pedro'.split(','))
---
+++ exercicios_js/fundamentos/templateString.js
@@ -0,0 +1,6 @@
+const nome = 'Rebeca'
+const concatenacao = 'Ola ' + nome + '!'
+const template =`
+Ola
+${nome}!`
+console.log(concatenacao , template)
