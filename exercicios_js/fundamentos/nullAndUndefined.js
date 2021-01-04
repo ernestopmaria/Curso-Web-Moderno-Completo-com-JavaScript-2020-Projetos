6d5f85cae4792640f1994442228792198ef28122
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-01-04 14:57:45 +0100

    Update nullAndUndefined.js

--- exercicios_js/fundamentos/nullAndUndefined.js
+++ exercicios_js/fundamentos/nullAndUndefined.js
@@ -1,2 +1,21 @@
-let valor 
+let valor  //variavel não inicializada
 console.log(valor)
+
+
+  valor = null // ausencia de valor
+  console.log(valor)
+
+  //console.log(valor.toString()) //erro
+
+const produto ={}
+  console.log(produto.preco)
+  console.log(produto)
+  produto.preco = 3.50
+  console.log(produto.preco)
+  produto.preco = undefined
+  console.log(!!produto.preco)
+  console.log(produto)
+
+  produto.preco = null
+  console.log(!!produto.preco)
+  console.log(produto)
