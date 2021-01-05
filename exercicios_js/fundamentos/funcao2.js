514d161acbd9687829781db4ce65111d75931fe9
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-01-05 09:05:35 +0100

    funtion

---
+++ exercicios_js/fundamentos/funcao2.js
@@ -0,0 +1,12 @@
+//Armazenando uma função dentro de uma variavel
+const imprimirSoma = function(a, b){
+    console.log(a + b)
+
+}
+imprimirSoma(2, 3)
+
+//armazenamento uma funcao arrow em uma variavel
+const soma = (a, b)=>{
+    return a + b
+}
+console.log(soma(2, 3 ))
